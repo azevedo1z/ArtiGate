f296e762ba09db2860132026021aec16bbdc18ed
azevedo1x <gabriel.azeve04@gmail.com>
2025-03-05 15:39:19 -0300

    fixing objects

--- backend-api/src/app/domain/repositories/user.repositoryImplementation.ts
+++ backend-api/src/app/domain/repositories/user.repositoryImplementation.ts
@@ -1,12 +1,13 @@
 import { User } from "@prisma/client";
 import { PrismaClient } from "@prisma/client/extension";
 import { CreateUserDTO } from "../../applications/dtos/user/createUser.dto";
+import { UserRepository } from "./user.repository";
 
-export class UserRepository {
+export class UserRepositoryImplementation implements UserRepository {
   constructor(private readonly prisma: PrismaClient) {}
 
   async findByEmail(email: string): Promise<User | null> {
-    return this.prisma.findUnique({
+    return this.prisma.user.findUnique({
       where: { email },
     });
   }

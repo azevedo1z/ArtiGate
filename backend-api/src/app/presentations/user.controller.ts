1a27995f842051cea57ab2b2e960a68f48c5c413
azevedo1x <gabriel.azeve04@gmail.com>
2025-03-06 17:16:51 -0300

    updating app.module, main for swagger and implementing the createuser router

--- backend-api/src/app/presentations/user.controller.ts
+++ backend-api/src/app/presentations/user.controller.ts
@@ -1,10 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { CreateUserService } from '../applications/services/user/createUser.service';
+import * as createUserDTO from '../applications/dtos/user/createUser.dto';
 
 @Controller('user')
-export class UserController{
-constructor(private readonly createUserService: CreateUserService){}
+export class UserController {
+  constructor(private readonly createUserService: CreateUserService) {}
 
-
-
+  @Post('create')
+  async create(@Body() data: createUserDTO.CreateUserDTO) {
+    return this.createUserService.execute(data);
+  }
 }

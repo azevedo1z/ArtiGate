a471ae8e31a5ed93346843c8237ca205efc7c878
azevedo1x <gabriel.azeve04@gmail.com>
2025-03-07 10:13:32 -0300

    adding prisma service

--- backend-api/src/app/domain/repositories/user.repositoryImplementation.ts
+++ backend-api/src/app/domain/repositories/user.repositoryImplementation.ts
@@ -1,10 +1,10 @@
 import { User } from "@prisma/client";
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaService } from "../../infrastructure/prisma.service";
 import { CreateUserDTO } from "../../applications/dtos/user/createUser.dto";
 import { UserRepository } from "./user.repository";
 
 export class UserRepositoryImplementation implements UserRepository {
-  constructor(private readonly prisma: PrismaClient) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   async findByEmail(email: string): Promise<User | null> {
     return this.prisma.user.findUnique({

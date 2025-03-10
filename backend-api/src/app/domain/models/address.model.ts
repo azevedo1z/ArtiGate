21e0ae1d340688d1294241d9841e6ef550a0b888
Gabriel Azevedo <96775983+azevedo1x@users.noreply.github.com>
2025-03-10 15:34:19 -0300

    address model setters and add controller

--- backend-api/src/app/domain/models/address.model.ts
+++ backend-api/src/app/domain/models/address.model.ts
@@ -1,65 +1,83 @@
 export class Address {
-    static readonly COUNTRY = 'Brasil';
-  
-    private _id: string;
-    private _zipCode: string;
-    private _street: string;
-    private _complement?: string;
-    private _neighborhood: string;
-    private _city: string;
-    private _state: string;
-    private _country: string;
-  
-    constructor(
-      id: string,
-      zipCode: string,
-      street: string,
-      neighborhood: string,
-      city: string,
-      state: string,
-      country: string = Address.COUNTRY,
-      complement?: string
-    ) {
-      this._id = id;
-      this._zipCode = zipCode;
-      this._street = street;
-      this._neighborhood = neighborhood;
-      this._city = city;
-      this._state = state;
-      this._country = country;
-      this._complement = complement;
-    }
-  
-    get id(): string {
-      return this._id;
-    }
-  
-    get zipCode(): string {
-      return this._zipCode;
-    }
-  
-    get street(): string {
-      return this._street;
-    }
-  
-    get complement(): string | undefined {
-      return this._complement;
-    }
-  
-    get neighborhood(): string {
-      return this._neighborhood;
-    }
-  
-    get city(): string {
-      return this._city;
-    }
-  
-    get state(): string {
-      return this._state;
-    }
-  
-    get country(): string {
-      return this._country;
-    }
+  static readonly COUNTRY = 'Brasil';
+
+  private _id: string;
+  private _zipCode: string;
+  private _street: string;
+  private _complement?: string;
+  private _neighborhood: string;
+  private _city: string;
+  private _state: string;
+  private _country: string;
+
+  constructor(
+    id: string,
+    zipCode: string,
+    street: string,
+    neighborhood: string,
+    city: string,
+    state: string,
+    country: string = Address.COUNTRY,
+    complement?: string
+  ) {
+    this._id = id;
+    this._zipCode = zipCode;
+    this._street = street;
+    this._neighborhood = neighborhood;
+    this._city = city;
+    this._state = state;
+    this._country = country;
+    this._complement = complement;
   }
+
+  get id(): string {
+    return this._id;
+  }
+
+  get zipCode(): string {
+    return this._zipCode;
+  }
+  private set zipCode(value: string) {
+    this._zipCode = value;
+  }
+
+  get street(): string {
+    return this._street;
+  }
+  private set street(value: string) {
+    this._street = value;
+  }
+
+  get complement(): string | undefined {
+    return this._complement;
+  }
+  private set complement(value: string | undefined) {
+    this._complement = value;
+  }
+
+  get neighborhood(): string {
+    return this._neighborhood;
+  }
+  private set neighborhood(value: string) {
+    this._neighborhood = value;
+  }
+
+  get city(): string {
+    return this._city;
+  }
+  private set city(value: string) {
+    this._city = value;
+  }
+
+  get state(): string {
+    return this._state;
+  }
+  private set state(value: string) {
+    this._state = value;
+  }
+
+  get country(): string {
+    return this._country;
+  }
+}
   
---
+++ backend-api/src/app/presentations/address.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from "@nestjs/common";
+import { CreateAddressDTO } from "../applications/dtos/address/createAddress.dto";
+import { CreateAddressService } from "../applications/services/address/createAddress.service";
+
+@Controller('address')
+export class AddressController {
+    constructor(private readonly createAddressService: CreateAddressService) {}
+
+    @Post('create')
+    async create(@Body() data: CreateAddressDTO) {
+        return this.createAddressService.execute(data);
+    }
+}

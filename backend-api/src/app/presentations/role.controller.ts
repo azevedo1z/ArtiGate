204c63edf5767b5bec65b50d34d4a286d4ec7081
Gabriel Azevedo <96775983+azevedo1x@users.noreply.github.com>
2025-03-26 20:12:57 -0300

    updating role repository and controller and implementing the get all roles method

--- backend-api/src/app/presentations/role.controller.ts
+++ backend-api/src/app/presentations/role.controller.ts
@@ -1,13 +1,27 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateRoleService } from '../applications/services/role/createRole.service';
 import { CreateRoleDTO } from '../applications/dtos/role/createRole.dto';
+import { GetRoleService } from '../applications/services/role/getRole.service';
 
 @Controller('role')
 export class RoleController {
-  constructor(private readonly createRoleService: CreateRoleService) {}
+  constructor(
+    private readonly createRoleService: CreateRoleService,
+    private readonly getRoleService: GetRoleService
+  ) {}
 
   @Post('create')
   async create(@Body() data: CreateRoleDTO) {
     return this.createRoleService.execute(data);
+  }
+
+  @Get('allRoles')
+  async getAll() {
+    return this.getRoleService.getAll();
+  }
+
+  @Get(':id')
+  async getById(@Param('id') id: string) {
+    return this.getRoleService.getById(id);
   }
 }

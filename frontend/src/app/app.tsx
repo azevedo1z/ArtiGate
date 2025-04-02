3bfdadf07a5d85a288c820b6230ea4c5125e3f1f
azevedo1x <gabriel.azeve04@gmail.com>
2025-04-02 20:51:25 -0300

    removal of nx boilerplates

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -1,12 +1,8 @@
-import NxWelcome from './nx-welcome';
-
 import { Route, Routes, Link } from 'react-router-dom';
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="@arti-gate/frontend" />
-
       {/* START: routes */}
       {/* These routes and navigation have been generated for you */}
       {/* Feel free to move and update them to fit your needs */}

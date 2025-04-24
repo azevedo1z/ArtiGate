c9a00e4b1aed9c71d5e696a49ca2fe163965f584
azevedo1x <gabriel.azeve04@gmail.com>
2025-04-24 16:32:36 -0300

    add authguards

---
+++ backend-api/src/app/infrastructure/authGuard.service.ts
@@ -0,0 +1,45 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Observable } from 'rxjs';
+import { Request } from 'express';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private readonly jwtService: JwtService) {}
+
+  canActivate(
+    context: ExecutionContext
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+
+    return this.validateRequest(request);
+  }
+
+  private async validateRequest(request: Request): Promise<boolean> {
+    const authHeader = request.headers['authorization'];
+
+    if (authHeader == null) throw new Error('AuthHeader is empty.');
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+      const secretKey = process.env.SECRET_KEY;
+
+      if (secretKey == null)
+        throw new Error(
+          'SECRET_KEY is not defined in the environment variables'
+        );
+
+      await this.jwtService.verify(token, { secret: secretKey });
+
+      return true;
+    } catch {
+      throw new UnauthorizedException('Invalid or expired token');
+    }
+  }
+}

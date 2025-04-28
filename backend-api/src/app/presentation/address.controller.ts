8ccfb61524d150d63e4df36b00fd2da0b6f136bb
azevedo1x <gabriel.azeve04@gmail.com>
2025-04-28 20:26:04 -0300

    wip setting await operators + update/put request

--- backend-api/src/app/presentation/address.controller.ts
+++ backend-api/src/app/presentation/address.controller.ts
@@ -1,33 +1,51 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
 import { CreateAddressDTO } from '../application/dtos/address/createAddress.dto';
 import { CreateAddressService } from '../application/services/address/createAddress.service';
 import { GetAddressService } from '../application/services/address/getAddress.service';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { AuthGuardService } from '../infrastructure/authGuard.service';
+import { UpdateAddressDTO } from '../application/dtos/address/updateAddress.dto';
+import { UpdateAddressService } from '../application/services/address/updateAddress.service';
 
 @Controller('address')
 export class AddressController {
   constructor(
     private readonly createAddressService: CreateAddressService,
-    private readonly getAddressService: GetAddressService
+    private readonly getAddressService: GetAddressService,
+    private readonly updateAddressService: UpdateAddressService
   ) {}
 
   @Post('create')
   async create(@Body() data: CreateAddressDTO) {
-    return this.createAddressService.execute(data);
+    return await this.createAddressService.execute(data);
+  }
+
+  @Put('update')
+  async update(@Body() data: UpdateAddressDTO) {
+    const address = await this.getAddressService.getById(data.id);
+
+    return this.updateAdressService.execute(data);
   }
 
   @Get('allAddresses')
   @ApiBearerAuth()
   @UseGuards(AuthGuardService)
   async getAll() {
-    return this.getAddressService.getAll();
+    return await this.getAddressService.getAll();
   }
 
   @Get(':id')
   @ApiBearerAuth()
   @UseGuards(AuthGuardService)
   async getById(@Param('id') id: string) {
-    return this.getAddressService.getById(id);
+    return await this.getAddressService.getById(id);
   }
 }

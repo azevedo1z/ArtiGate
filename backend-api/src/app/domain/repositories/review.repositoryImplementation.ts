35c4e69ddd03e16b9e6a3bdd03913909605e0cef
azevedo1x <gabriel.azeve04@gmail.com>
2025-05-05 15:32:36 -0300

    including update logic in review repository

--- backend-api/src/app/domain/repositories/review.repositoryImplementation.ts
+++ backend-api/src/app/domain/repositories/review.repositoryImplementation.ts
@@ -3,6 +3,7 @@ import { PrismaService } from '../../infrastructure/prisma.service';
 import { ReviewRepository } from './review.repository';
 import { Injectable } from '@nestjs/common';
 import { CreateReviewDTO } from '../../application/dtos/review/createReview.dto';
+import { UpdateReviewDTO } from '../../application/dtos/review/updateReview.dto';
 
 @Injectable()
 export class ReviewRepositoryImplementation implements ReviewRepository {
@@ -18,5 +19,17 @@ export class ReviewRepositoryImplementation implements ReviewRepository {
 
   async findAll(): Promise<Review[]> {
     return await this.prisma.review.findMany();
+  }
+
+  async update(data: UpdateReviewDTO): Promise<Review> {
+    //TODO: Globalize this
+    const dataToUpdate = Object.fromEntries(
+      Object.entries(data).filter(([, value]) => value !== undefined)
+    );
+
+    return await this.prisma.review.update({
+      where: { id: data.id },
+      data: dataToUpdate,
+    });
   }
 }

6fa58f1fb5f4bb6deb6f60783472bda5e046e0b4
azevedo1x <gabriel.azeve04@gmail.com>
2025-05-06 15:12:02 -0300

    creation of delete review service

---
+++ backend-api/src/app/application/services/review/deleteReview.service.ts
@@ -0,0 +1,27 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { ReviewRepository } from '../../../domain/repositories/review.repository';
+import { GetReviewService } from './getReview.service';
+import { GetUserService } from '../user/getUser.service';
+
+@Injectable()
+export class DeleteReviewService {
+  constructor(
+    private readonly repository: ReviewRepository,
+    private readonly getReviewService: GetReviewService,
+    private readonly getUserService: GetUserService
+  ) {}
+
+  async execute(id: string): Promise<boolean> {
+    await this.getReviewService.getBydId(id);
+
+    const reviewers = await this.getUserService.getByReviewId(id);
+
+    const hasConstraint =
+      reviewers?.some((reviewer) => reviewer.id === id) ?? false;
+
+    if (hasConstraint)
+      throw new BadRequestException('The review is associated with a user.');
+
+    return await this.repository.delete(id);
+  }
+}

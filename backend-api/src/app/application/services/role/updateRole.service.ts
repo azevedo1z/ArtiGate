b2782cd4b534b5b01c4b030308b621fc430058cb
azevedo1x <gabriel.azeve04@gmail.com>
2025-05-07 15:49:30 -0300

    creation of updateroleservice + appmodule inclusion of it

---
+++ backend-api/src/app/application/services/role/updateRole.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { Role } from '../../../domain/models/role.model';
+import { RoleRepository } from '../../../domain/repositories/role.repository';
+import { UpdateRoleDTO } from '../../dtos/role/updateRole.dto';
+
+@Injectable()
+export class UpdateRoleService {
+  constructor(private readonly repository: RoleRepository) {}
+
+  async execute(data: UpdateRoleDTO): Promise<Role> {
+    await this.repository.findById(data.id);
+
+    const roleRecord = await this.repository.update(data);
+
+    return Role.factory(roleRecord.id, roleRecord.name);
+  }
+}

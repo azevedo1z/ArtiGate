1225c2bdb95478e2d3cc2cde059d8024a6fc9e51
azevedo1x <gabriel.azeve04@gmail.com>
2025-05-13 08:39:10 -0300

    adding review constraint to delete article service

--- backend-api/src/app/application/services/article/deleteArticle.service.ts
+++ backend-api/src/app/application/services/article/deleteArticle.service.ts
@@ -2,22 +2,27 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { ArticleRepository } from '../../../domain/repositories/article.repository';
 import { GetArticleService } from './getArticle.service';
 import { GetUserService } from '../user/getUser.service';
+import { GetReviewService } from '../review/getReview.service';
 
 @Injectable()
 export class DeleteArticleService {
   constructor(
     private readonly articleRepository: ArticleRepository,
     private readonly getArticleService: GetArticleService,
-    private readonly getUserService: GetUserService
+    private readonly getUserService: GetUserService,
+    private readonly getReviewService: GetReviewService
   ) {}
 
   async execute(id: string): Promise<boolean> {
     await this.getArticleService.getById(id);
 
-    const authors = await this.getUserService.getByArticleId(id);
+    let hasConstraint = false;
 
-    const hasConstraint =
-      authors?.some((author) => author.articleId === id) ?? false;
+    const authors = await this.getUserService.getByArticleId(id);
+    const reviews = await this.getReviewService.getByArticleId(id);
+
+    if (authors?.some((author) => author.articleId === id))
+      hasConstraint = true;
 
     if (hasConstraint)
       throw new BadRequestException('The article is associated with a user.');

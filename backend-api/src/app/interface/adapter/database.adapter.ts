b50bb3b7738bc2e8ab2c25bec41b0864bbe5977c
azevedo1x <gabriel.azeve04@gmail.com>
2025-05-15 19:38:08 -0300

    updating adapter to englobe usercreation and update

--- backend-api/src/app/interface/adapter/database.adapter.ts
+++ backend-api/src/app/interface/adapter/database.adapter.ts
@@ -1,7 +1,19 @@
 export abstract class DatabaseAdapter<T> {
   abstract findById(id: string): Promise<T | null>;
   abstract findAll(): Promise<T[]>;
-  abstract create(data: Partial<T>): Promise<T>;
-  abstract update(data: Partial<T>): Promise<T>;
+
+  abstract create(
+    data: Partial<T>,
+    primaryContextParam?: string,
+    secondaryContextParam?: string
+  ): Promise<T>;
+
+  abstract update(
+    data: Partial<T>,
+    primaryContextParam?: string | undefined,
+    secondaryContextParam?: string | undefined,
+    tertiaryContextParam?: boolean
+  ): Promise<T>;
+
   abstract delete(id: string): Promise<boolean>;
 }

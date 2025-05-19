e41bf9625419d59bc0b0bb6d7062d21bac52edef
azevedo1x <gabriel.azeve04@gmail.com>
2025-05-19 16:24:46 -0300

    updating role service  to interact with database adapter instead of repo

--- backend-api/src/app/application/services/role/getRole.service.ts
+++ backend-api/src/app/application/services/role/getRole.service.ts
@@ -1,13 +1,13 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
-import { RoleRepository } from '../../../infrastructure/repositories/role.repository';
 import { Role } from '../../../domain/models/role.model';
+import { DatabaseAdapter } from '../../../interface/adapter/database.adapter';
 
 @Injectable()
 export class GetRoleService {
-  constructor(private readonly repository: RoleRepository) {}
+  constructor(private readonly adapter: DatabaseAdapter<Role>) {}
 
   async getById(id: string): Promise<Role | null> {
-    const existingRole = await this.repository.findById(id);
+    const existingRole = await this.adapter.findBy(id);
 
     if (existingRole == null)
       throw new BadRequestException(`There is no role with the ID "${id}".`);
@@ -16,7 +16,7 @@ export class GetRoleService {
   }
 
   async getAll(): Promise<Role[]> {
-    const roles = await this.repository.findAll();
+    const roles = await this.adapter.findAll();
 
     return roles.map((existingRole) =>
       Role.factory(existingRole.id, existingRole.name)

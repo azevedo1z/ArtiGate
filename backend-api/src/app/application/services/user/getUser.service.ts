9fc1432d4aa18365b4204b8cc624927023c2d6e2
azevedo1x <gabriel.azeve04@gmail.com>
2025-05-20 22:17:38 -0300

    style: update naming changes

--- backend-api/src/app/application/services/user/getUser.service.ts
+++ backend-api/src/app/application/services/user/getUser.service.ts
@@ -7,7 +7,7 @@ export class GetUserService {
   constructor(private readonly adapter: DatabaseAdapter<User>) {}
 
   async getById(id: string): Promise<User | null> {
-    const existingUser = await this.adapter.findBy(id);
+    const existingUser = await this.adapter.findById(id);
 
     if (existingUser == null)
       throw new BadRequestException(`There is no user with the ID "${id}".`);
@@ -38,71 +38,6 @@ export class GetUserService {
         existingUser.badgeUrl,
         existingUser.passwordHash
       )
-    );
-  }
-
-  async getByEmail(email: string): Promise<User> {
-    const existingUser = await this.adapter.findBy(email);
-
-    if (existingUser == null)
-      throw new BadRequestException(
-        `There is no user with the E-mail "${email}".`
-      );
-
-    return User.factory(
-      existingUser.id,
-      existingUser.name,
-      existingUser.email,
-      existingUser.phone,
-      existingUser.homeAddressId,
-      existingUser.jobAddressId,
-      existingUser.badgeUrl,
-      existingUser.passwordHash
-    );
-  }
-
-  async getByAddressId(addressId: string): Promise<User> {
-    const existingUser = await this.adapter.findBy(addressId);
-
-    if (existingUser == null)
-      throw new BadRequestException(
-        `There is no user with the address Id "${addressId}".`
-      );
-
-    return User.factory(
-      existingUser.id,
-      existingUser.name,
-      existingUser.email,
-      existingUser.phone,
-      existingUser.homeAddressId,
-      existingUser.jobAddressId,
-      existingUser.badgeUrl,
-      existingUser.passwordHash
-    );
-  }
-
-  async getByReviewId(reviewId: string): Promise<User> {
-    if (!this.adapter.findByReviewId)
-      throw new BadRequestException(
-        'Database adapter is not properly configured.'
-      );
-
-    const reviewer = await this.adapter.findByReviewId(reviewId);
-
-    if (reviewer == null)
-      throw new BadRequestException(
-        `There is no user with the review Id "${reviewId}".`
-      );
-
-    return User.factory(
-      reviewer.id,
-      reviewer.name,
-      reviewer.email,
-      reviewer.phone,
-      reviewer.homeAddressId,
-      reviewer.jobAddressId,
-      reviewer.badgeUrl,
-      reviewer.passwordHash
     );
   }
 }

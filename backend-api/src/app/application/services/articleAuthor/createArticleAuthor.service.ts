2383fc079e92b3fa93e59d88127cde68c9694500
azevedo1x <gabriel.azeve04@gmail.com>
2025-05-28 20:35:01 -0300

    chore: separation articleAuthor use cases of article

---
+++ backend-api/src/app/application/services/articleAuthor/createArticleAuthor.service.ts
@@ -0,0 +1,17 @@
+import { ArticleAuthorDatabaseAdapter } from '../../../interface/adapter/database.adapter';
+import { CreateArticleDTO } from '../../dtos/article/createArticle.dto';
+
+export class CreateArticleAuthorService {
+  constructor(private readonly adapter: ArticleAuthorDatabaseAdapter) {}
+
+  async execute(data: CreateArticleDTO, articleId: string) {
+    for (const userId of data.authorIds) {
+      const articleAuthorData = {
+        articleId,
+        userId,
+      };
+
+      await this.adapter.create(articleAuthorData);
+    }
+  }
+}

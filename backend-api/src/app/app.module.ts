b74e02fc3327a05356b73ca2b50d2071e7370d80
azevedo1x <gabriel.azeve04@gmail.com>
2025-05-27 21:19:00 -0300

    feat: adding article author controller for principle segregation + app module inclusion

--- backend-api/src/app/app.module.ts
+++ backend-api/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { AddressController } from './interface/controllers/address.controller';
 import { RoleController } from './interface/controllers/role.controller';
 import { ArticleController } from './interface/controllers/article.controller';
 import { ReviewController } from './interface/controllers/review.controller';
+import { ArticleAuthorController } from './interface/controllers/articleAuthor.controller';
 import { UserRepository } from './infrastructure/repositories/user.repository';
 import { AddressRepository } from './infrastructure/repositories/address.repository';
 import { RoleRepository } from './infrastructure/repositories/role.repository';
@@ -60,6 +61,7 @@ import { GetUserRoleService } from './application/services/user/getUserRole.serv
     RoleController,
     ArticleController,
     ReviewController,
+    ArticleAuthorController,
   ],
   providers: [
     PrismaService,

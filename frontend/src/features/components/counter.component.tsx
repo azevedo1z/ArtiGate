fb836e2f37706bb482d219edf7dc9d269c09d8b2
azevedo1x <gabriel.azeve04@gmail.com>
2025-05-30 09:20:42 -0300

    chore: adding counter slice + it's component

---
+++ frontend/src/features/components/counter.component.tsx
@@ -0,0 +1,17 @@
+// src/components/Counter.tsx
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from '../../store/counter.slice';
+import { RootState } from '../../store/my.store';
+
+export default function Counter() {
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => dispatch(increment())}>+1</button>
+      <button onClick={() => dispatch(decrement())}>-1</button>
+    </div>
+  );
+}

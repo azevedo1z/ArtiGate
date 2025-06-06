65a1b10a76861f838db1965231e122c27bb22b34
azevedo1x <gabriel.azeve04@gmail.com>
2025-06-06 10:57:16 -0300

    fix: import directory

--- frontend/src/components/counter.component.tsx
+++ frontend/src/components/counter.component.tsx
@@ -1,7 +1,7 @@
 // src/components/Counter.tsx
 import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement } from '../../store/counter.slice';
-import { RootState } from '../../store/my.store';
+import { increment, decrement } from '../store/counter.slice';
+import { RootState } from '../store/my.store';
 
 export default function Counter() {
   const count = useSelector((state: RootState) => state.counter.value);

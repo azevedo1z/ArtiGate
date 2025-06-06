e7759ee8bf41bd378c1202cc46eb8cb2e2a38ee6
azevedo1x <gabriel.azeve04@gmail.com>
2025-06-06 15:24:19 -0300

    chore: updating counter component to match best practices

--- frontend/src/components/counter.component.tsx
+++ frontend/src/components/counter.component.tsx
@@ -1,17 +1,18 @@
-// src/components/Counter.tsx
-import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement } from '../store/counter.slice';
-import { RootState } from '../store/my.store';
+import React, { useState } from 'react';
 
-export default function Counter() {
-  const count = useSelector((state: RootState) => state.counter.value);
-  const dispatch = useDispatch();
+const Counter: React.FC = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount((c) => c + 1);
+  const decrement = () => setCount((c) => c - 1);
 
   return (
     <div>
-      <p>Count: {count}</p>
-      <button onClick={() => dispatch(increment())}>+1</button>
-      <button onClick={() => dispatch(decrement())}>-1</button>
+      <h2>Counter: {count}</h2>
+      <button onClick={decrement}>-</button>
+      <button onClick={increment}>+</button>
     </div>
   );
-}
+};
+
+export default Counter;

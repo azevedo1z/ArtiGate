ae0ec3a3dfbe7259ff530596d07e96be851f9d42
azevedo1x <gabriel.azeve04@gmail.com>
2025-06-18 17:11:30 -0300

    feat: using react router to the app.tsx

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -1,13 +1,15 @@
-import Button from '../components/button.component';
+import { Route, Routes } from 'react-router-dom';
 import Layout from '../components/layout.component';
+import HomePage from './pages/home.page';
+import LoginPage from './pages/login.page';
 
 export function App() {
   return (
     <Layout>
-      <div className="flex flex-col items-center justify-center min-h-[60vh]">
-        <h1>Join now.</h1>
-        <Button>Log in</Button>
-      </div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
     </Layout>
   );
 }

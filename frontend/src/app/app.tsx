f09c96892e6d68622fb5b8a9a28f74bca04d564a
azevedo1x <gabriel.azeve04@gmail.com>
2025-06-18 16:00:37 -0300

    chore: removing unnecessary components at app.tsx

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -1,19 +1,13 @@
 import Button from '../components/button.component';
-import Input from '../components/input.component';
 import Layout from '../components/layout.component';
-import TextArea from '../components/textarea.component';
 
 export function App() {
   return (
     <Layout>
-      <div>
-        <h1>Redux Starter</h1>
-        <div>
-          <Button>Log in</Button>
-          <TextArea label={'Main Text Area'} placeholder={'Type here...'} />
-        </div>
+      <div className="flex flex-col items-center justify-center min-h-[60vh]">
+        <h1>Join now.</h1>
+        <Button>Log in</Button>
       </div>
-      <Input label={'Input'} placeholder={'Type here too...'} />
     </Layout>
   );
 }

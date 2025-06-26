9c7b210e9b0ea6a6f87ee225d769d5daf86aad4f
azevedo1x <gabriel.azeve04@gmail.com>
2025-06-26 14:07:25 -0300

    style: formatting raw login page

--- frontend/src/app/pages/login.page.tsx
+++ frontend/src/app/pages/login.page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const LoginPage: React.FC = () => {
-  return (<div>
-  <h1> Welcome to ArtiGate.</h1>
-  <div></div>
-  </div>);
+  return (
+    <div>
+      <h1> Welcome to ArtiGate.</h1>
+      <div></div>
+    </div>
+  );
 };
 
 export default LoginPage;

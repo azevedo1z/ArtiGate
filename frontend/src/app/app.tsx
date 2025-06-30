cff7e9de8748b51b29990aa43156fcf41b392f8a
azevedo1x <gabriel.azeve04@gmail.com>
2025-06-30 15:50:37 -0300

    feat: toaster provider and it's inclusion to the app.tsx

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -1,18 +1,21 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from '../components/layout.component';
+import Toaster from '../providers/toaster.provider';
 import HomePage from '../pages/home.page';
 import LoginPage from '../pages/login.page';
 import SignUpPage from '../pages/signup.page';
 
 export function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-      </Routes>
-    </Layout>
+    <Toaster>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+        </Routes>
+      </Layout>
+    </Toaster>
   );
 }
 

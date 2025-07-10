f01ffe9d065aeb657aa07337b6aa0756dcce02ae
azevedo1x <gabriel.azeve04@gmail.com>
2025-07-10 13:40:08 -0300

    fix: bad naming routes + absence of params in requests

--- backend-api/src/app/interface/controllers/article.controller.ts
+++ backend-api/src/app/interface/controllers/article.controller.ts
@@ -74,7 +74,7 @@ export class ArticleController {
     }
   }
 
-  @Get('by/{authorId}')
+  @Get(':authorId')
   async getByAuthorId(@Param('authorId') authorId: string) {
     try {
       return await this.getArticleService.getByAuthorId(authorId);

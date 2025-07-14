6076a7bbd055f746defa96189039a62fd2148d6f
azevedo1x <gabriel.azeve04@gmail.com>
2025-07-14 17:13:24 -0300

    chore: creation of constants and helpers utils

---
+++ frontend/src/utils/constants.util.tsx
@@ -0,0 +1,10 @@
+const roleOptions = [
+    { value: 'AUTHOR', label: 'Author', disabled: true },
+    { value: 'REVIEWER', label: 'Reviewer' },
+  ];
+
+  const cardBrandOptions = [
+    { value: 'Visa', label: 'Visa' },
+    { value: 'Mastercard', label: 'Mastercard' },
+    { value: 'American Express', label: 'American Express' },
+  ];

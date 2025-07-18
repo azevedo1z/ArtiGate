d94fa281feca85310bd6ba26e50932b9d91008cb
azevedo1x <gabriel.azeve04@gmail.com>
2025-07-18 16:04:38 -0300

    chore: adding types and updating pages to use it

--- frontend/src/shared/types/types.shared.tsx
+++ frontend/src/shared/types/types.shared.tsx
@@ -1,3 +1,16 @@
+export interface SignUpResponse {
+  access_token: string;
+  statusCode: number;
+  message: string;
+  error: string;
+}
+
+export interface LoginResponse {
+  access_token: string;
+  statusCode: number;
+  message: string;
+  error: string;
+}
 export interface UserData {
   id: string;
   name: string;
@@ -7,4 +20,37 @@ export interface UserData {
 export interface UserRoleData {
   roleId: string;
   roleName: string;
+}
+
+export interface RoleData {
+  _id: string;
+  _name: string;
+}
+
+export interface SignUpFormData {
+  name: string;
+  email: string;
+  phone: string;
+  password: string;
+  passwordConfirmation: string;
+  roles: string[];
+
+  homeZipCode: string;
+  homeStreet: string;
+  homeComplement: string;
+  homeNeighborhood: string;
+  homeCity: string;
+  homeState: string;
+
+  jobZipCode: string;
+  jobStreet: string;
+  jobComplement: string;
+  jobNeighborhood: string;
+  jobCity: string;
+  jobState: string;
+
+  // mocked
+  cardNumber: string;
+  cardExpiry: string;
+  cardBrand: string;
 }

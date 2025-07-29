963dc782a8a48e4091bd6abe2693c0e1153b2ca8
azevedo1x <gabriel.azeve04@gmail.com>
2025-07-29 13:21:32 -0300

    feat: updating store slice to use the userslice

--- frontend/src/store/my.store.ts
+++ frontend/src/store/my.store.ts
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+
 import authReducer from './slices/auth.slice';
+import userReducer from './slices/user.slice';
 
 const myStore = configureStore({
   reducer: {
     auth: authReducer,
+    user: userReducer,
   },
 });
 

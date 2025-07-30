a5845b1e719d3a09261eb10fc52daed04c952802
azevedo1x <gabriel.azeve04@gmail.com>
2025-07-30 17:00:45 -0300

    feat: updating the redux store to use the newly created roles reducer

--- frontend/src/store/my.store.ts
+++ frontend/src/store/my.store.ts
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import authReducer from './slices/auth.slice';
 import userReducer from './slices/user.slice';
+import rolesReducer from './slices/roles.slice';
 
 const myStore = configureStore({
   reducer: {
     auth: authReducer,
     user: userReducer,
+    roles: rolesReducer,
   },
 });
 

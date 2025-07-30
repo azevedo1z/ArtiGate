62853acf69c95dfda78e9fda0452e4e88ead1f24
azevedo1x <gabriel.azeve04@gmail.com>
2025-07-30 16:57:53 -0300

    chore: removing userId prop from auth slice

--- frontend/src/store/slices/auth.slice.ts
+++ frontend/src/store/slices/auth.slice.ts
@@ -2,7 +2,6 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   isLoggedIn: false,
-  userId: null,
 };
 
 const authSlice = createSlice({
@@ -11,11 +10,9 @@ const authSlice = createSlice({
   reducers: {
     login(state, action) {
       state.isLoggedIn = true;
-      state.userId = action.payload.userId;
     },
     logout(state) {
       state.isLoggedIn = false;
-      state.userId = null;
     },
   },
 });

c28d3cb14596cadac61fc3301fc0c42ff7c665f8
azevedo1x <gabriel.azeve04@gmail.com>
2025-08-07 16:32:49 -0300

    style: updating imports of landing page

--- frontend/src/pages/landing.page.tsx
+++ frontend/src/pages/landing.page.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from '../components/button.component';
 import Container from '../components/container.component';

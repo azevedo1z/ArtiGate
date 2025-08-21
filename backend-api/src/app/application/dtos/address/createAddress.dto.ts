074ca7fb09862346f564a4e2f04151c0afff5e5c
azevedo1x <gabriel.azeve04@gmail.com>
2025-08-21 11:13:54 -0300

    feat: add validation rules for address fields in CreateAddressDTO

--- backend-api/src/app/application/dtos/address/createAddress.dto.ts
+++ backend-api/src/app/application/dtos/address/createAddress.dto.ts
@@ -1,22 +1,42 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString, MinLength, MaxLength, IsOptional, Matches } from 'class-validator';
 
 export class CreateAddressDTO {
   @ApiProperty()
+  @IsString()
+  @Matches(/^\d{5}-?\d{3}$/, {
+    message: 'ZIP code must be in format XXXXX-XXX or XXXXXXXX'
+  })
   zipCode: string;
 
   @ApiProperty()
+  @IsString()
+  @MinLength(5)
+  @MaxLength(200)
   street: string;
 
   @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  @MaxLength(100)
   complement?: string;
 
   @ApiProperty()
+  @IsString()
+  @MinLength(2)
+  @MaxLength(100)
   neighborhood: string;
 
   @ApiProperty()
+  @IsString()
+  @MinLength(2)
+  @MaxLength(100)
   city: string;
 
   @ApiProperty()
+  @IsString()
+  @MinLength(2)
+  @MaxLength(100)
   state: string;
 
   constructor(

46d63fc9258a52f8cb89ecdd6bde72f30e31156f
azevedo1x <gabriel.azeve04@gmail.com>
2025-08-21 11:18:47 -0300

    feat: add validation rules for email and password fields in AuthUserDTO and CreateUserDTO

--- backend-api/src/app/application/dtos/user/authUser.dto.ts
+++ backend-api/src/app/application/dtos/user/authUser.dto.ts
@@ -1,10 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, MinLength, MaxLength } from 'class-validator';
 
 export class AuthUserDTO {
   @ApiProperty()
+  @IsEmail()
+  @MaxLength(255)
   email: string;
 
   @ApiProperty()
+  @IsString()
+  @MinLength(8)
+  @MaxLength(128)
   password: string;
 
   constructor(email: string, password: string) {
--- backend-api/src/app/application/dtos/user/createUser.dto.ts
+++ backend-api/src/app/application/dtos/user/createUser.dto.ts
@@ -1,20 +1,34 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, MinLength, MaxLength, IsArray, ArrayNotEmpty, IsUrl, Matches } from 'class-validator';
 import { CreateAddressDTO } from '../address/createAddress.dto';
 
 export class CreateUserDTO {
   @ApiProperty()
+  @IsString()
+  @MinLength(2)
+  @MaxLength(100)
   name: string;
 
   @ApiProperty()
+  @IsEmail()
+  @MaxLength(255)
   email: string;
 
   @ApiProperty()
+  @IsString()
+  @Matches(/^\+\d{1,3}-\d{3}-\d{3}-\d{4}$/, {
+    message: 'Phone must be in format +X-XXX-XXX-XXXX (international)'
+  })
   phone: string;
 
   @ApiProperty()
+  @IsUrl()
+  @MaxLength(500)
   badgeUrl: string;
 
   @ApiProperty()
+  @IsArray()
+  @ArrayNotEmpty()
   roleIds: string[];
 
   @ApiProperty()
@@ -24,6 +38,12 @@ export class CreateUserDTO {
   jobAddress: CreateAddressDTO;
 
   @ApiProperty()
+  @IsString()
+  @MinLength(8)
+  @MaxLength(128)
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/, {
+    message: 'Password must contain at least one uppercase letter, one lowercase letter, one number and one special character'
+  })
   password: string;
 
   constructor(

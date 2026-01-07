62a9931e3780c73ba9a3b1b23fc33c92392a2562
azevedo1x <gabriel.azeve04@gmail.com>
2026-01-06 21:22:32 -0300

    refactor: add migration for AUTHOR and REVIEWER roles in the Role table

---
+++ prisma/migrations/20260107000032_add_author_reviewer_roles/migration.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO "Role" ("id", "name", "createdOn") VALUES (gen_random_uuid(), 'AUTHOR', NOW());
+INSERT INTO "Role" ("id", "name", "createdOn") VALUES (gen_random_uuid(), 'REVIEWER', NOW());

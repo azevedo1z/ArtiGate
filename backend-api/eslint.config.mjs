010d4f912f605821a0d03f70aaaa7de408f978f4
azevedo1x <gabriel.azeve04@gmail.com>
2026-03-29 12:31:28 -0300

    feat(eslint): update ESLint configuration to disable no-explicit-any rule for test files and add unused-vars rule

--- backend-api/eslint.config.mjs
+++ backend-api/eslint.config.mjs
@@ -1,3 +1,11 @@
 import baseConfig from '../eslint.config.mjs';
 
-export default [...baseConfig];
+export default [
+  ...baseConfig,
+  {
+    files: ['**/*.spec.ts', '**/*.test.ts'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+];

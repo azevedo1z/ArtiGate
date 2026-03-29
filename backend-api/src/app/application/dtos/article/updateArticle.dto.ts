db774d098516e19c7536bed0dec961f90ee292a1
azevedo1x <gabriel.azeve04@gmail.com>
2026-03-29 12:29:48 -0300

    feat(dtos): update UpdateArticleDTO and UpdateReviewDTO to include optional scoreAvg and score properties with validation

--- backend-api/src/app/application/dtos/article/updateArticle.dto.ts
+++ backend-api/src/app/application/dtos/article/updateArticle.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsArray, IsOptional } from 'class-validator';
+import { IsString, IsArray, IsOptional, IsNumber } from 'class-validator';
 
 export class UpdateArticleDTO {
   @ApiProperty()
@@ -9,17 +9,28 @@ export class UpdateArticleDTO {
   @ApiProperty({ required: false })
   @IsString()
   @IsOptional()
-  summary: string;
+  summary?: string;
 
   @ApiProperty({ type: [String], required: false })
   @IsArray()
   @IsString({ each: true })
   @IsOptional()
-  authorIds: string[];
+  authorIds?: string[];
 
-  constructor(id: string, summary: string, authorIds: string[]) {
+  @ApiProperty({ required: false })
+  @IsNumber()
+  @IsOptional()
+  scoreAvg?: number;
+
+  constructor(
+    id: string,
+    summary?: string,
+    authorIds?: string[],
+    scoreAvg?: number
+  ) {
     this.id = id;
     this.summary = summary;
     this.authorIds = authorIds;
+    this.scoreAvg = scoreAvg;
   }
 }
